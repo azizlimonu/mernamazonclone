b7584945e55ad3c33b8651ca6f238f1dff07e862
Aziz Limonu <azizlimonu@gmail.com>
2022-12-05 17:10:02 +0800

    add react bootstrap and router bootstrap

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
 import { Route, Routes } from "react-router-dom";
 import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import { LinkContainer } from 'react-router-bootstrap';
 
 function App() {
   return (
-    <div>
+    <div className="d-flex flex-column site-container">
+      {/* header */}
       <header>
-        <a href="/">amazona</a>
+        <Navbar bg="dark" variant="dark">
+          <Container>
+            {/* brand */}
+            <LinkContainer to="/">
+              <Navbar.Brand>amazona</Navbar.Brand>
+            </LinkContainer>
+
+          </Container>
+        </Navbar>
       </header>
+
+      {/* main section */}
       <main>
-        <h1>Featured Products</h1>
-        <div className="products">
+        <Container>
           <Routes>
             <Route path='/' element={<HomeScreen />} />
             <Route path="/product/:slug" element={<ProductScreen />} />
           </Routes>
+        </Container>
+      </main >
 
-        </div>
-      </main>
-    </div>
+      {/* footer */}
+      <footer>
+        <div className="text-center">All rights reserved</div>
+      </footer>
+    </div >
   );
 }
 
